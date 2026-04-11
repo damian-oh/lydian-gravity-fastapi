cf411a383688ce930efd94ff5d670f9c3a30190d
damian-oh <damian@damianoh.com>
2026-04-11 18:23:34 -0400

    chore: import crud files in crud/__init__.py

---
+++ app/crud/__init__.py
@@ -0,0 +1,15 @@
+from . import (
+    crud_chord,
+    crud_melodic_note,
+    crud_song_section,
+    crud_song_sketch,
+    crud_user,
+)
+
+__all__ = [
+    "crud_chord",
+    "crud_melodic_note",
+    "crud_song_section",
+    "crud_song_sketch",
+    "crud_user",
+]
