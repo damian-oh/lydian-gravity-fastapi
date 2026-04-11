7e2bc77eadd23097a5e8b43953560fb4ba051946
damian-oh <damian@damianoh.com>
2026-04-11 15:41:37 -0400

    chore: add base.py to import database models

---
+++ app/db/base.py
@@ -0,0 +1,6 @@
+from app.db.base_class import Base
+from app.models.user import User
+from app.models.song_sketch import SongSketch
+from app.models.song_section import SongSection
+from app.models.chord import Chord
+from app.models.melodic_note import MelodicNote
