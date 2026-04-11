4ece0d55fe4c675c21f5ceeaef8b408d19a3e89f
damian-oh <damian@damianoh.com>
2026-04-11 16:09:50 -0400

    feat: add table creation to main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
 from app.core.config import settings
+from app.db.base import Base
+from app.db.session import engine
 
-app = FastAPI(title=settings.PROJECT_NAME, debug=settings.DEBUG)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
 
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    debug=settings.DEBUG,
+    lifespan=lifespan
+)
 
 @app.get("/")
 async def root():
