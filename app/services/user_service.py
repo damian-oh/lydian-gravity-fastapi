c76bc65bf8d01cbfa8407a43448ca890bda0c93d
damian-oh <damian@damianoh.com>
2026-04-18 19:54:40 -0400

    feat: update user_service.py to reflect the user authentication

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,14 +1,20 @@
 from sqlalchemy.orm import Session
 
-from app.core.security import get_password_hash, verify_password
+from app.core.security import (
+    get_password_hash,
+    verify_password,
+    verify_password_with_dummy,
+)
 from app.crud import crud_user
 from app.models.user import User
 from app.schemas.user import UserCreate, UserPasswordUpdate
 
+
 def create_user(db: Session, user_in: UserCreate) -> User:
     password_hash = get_password_hash(user_in.password)
-    
+
     return crud_user.create_user(db, user_in, password_hash)
+
 
 def change_user_password(
     db: Session,
@@ -22,6 +28,7 @@ def change_user_password(
 
     return crud_user.update_user_password(db, db_user, new_password_hash)
 
+
 def authenticate_user(
     db: Session,
     email: str,
@@ -30,6 +37,7 @@ def authenticate_user(
     db_user = crud_user.get_user_by_email(db, email)
 
     if db_user is None:
+        verify_password_with_dummy(password)
         return None
 
     if not verify_password(password, db_user.password_hash):
