082d1cef107d2d3199e943e27a140939b31cb17a
damian-oh <damian@damianoh.com>
2026-04-19 11:11:06 -0400

    chore: replace database generation with AsyncGenerator

--- app/db/session.py
+++ app/db/session.py
@@ -1,16 +1,19 @@
+from typing import AsyncGenerator
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
-from typing import Generator
+
 from app.core.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={"check_same_thread": False},
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_db() -> Generator[Session, None, None]:
+
+async def get_db() -> AsyncGenerator[Session, None]:
     try:
         db = SessionLocal()
         yield db
